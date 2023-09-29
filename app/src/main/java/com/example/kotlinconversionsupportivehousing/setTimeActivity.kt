dfd99fb0dbe1c15b618a26d2c1b35f0ac43fe6dc
Joel Anil John <anil.john13542@gmail.com>
2023-09-28 20:55:06 -0700

    Initial commit

---
+++ app/src/main/java/com/example/kotlinconversionsupportivehousing/setTimeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinconversionsupportivehousing
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SetTimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.set_date)
+    }
+}
